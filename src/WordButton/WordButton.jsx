ff7c0c6e92a10fe7a7ead153ea2ff7160abaaa69
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-01-12 14:04:31 +0000

    Fix keyboard shortcut bugs

--- src/WordButton/WordButton.jsx
+++ src/WordButton/WordButton.jsx
@@ -1,12 +1,19 @@
 import { useEffect } from "react";
 
 function WordButton(props) {
+    function keyPressListener(e) {
+        // return if key wasn't 1-4
+        if (e.key < 1 || e.key > 4) {
+            return;
+        }
+        if (e.key === (props.number + 1).toString()) {
+            props.answer(props.word);
+        }
+    }
     useEffect(() => {
-        window.addEventListener("keydown", (e) => {
-            if (e.key === props.number.toString()) {
-                props.answer(props.word);
-            }
-        })
+        window.addEventListener("keypress", keyPressListener)
+
+        return () => window.removeEventListener("keypress", keyPressListener)
     })
 
     return <button onClick={() => props.answer(props.word)}>{props.word}</button>;
