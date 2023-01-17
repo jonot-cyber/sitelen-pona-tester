25fcd7cf9f3fc3f8dd332c0d7477d51ea5292f31
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-01-17 00:19:58 +0000

    Re-add anti-cheat

--- src/Word.tsx
+++ src/Word.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Word(props: {word: string}) {
     return (
-        <div className="grid justify-center content-center grow">
+        <div className="grid justify-center content-center grow select-none">
             <h1 className="font-sitelen text-[48vh] leading-none md:text-[80vh] text-black dark:text-white">{props.word}</h1>
         </div>
     )
